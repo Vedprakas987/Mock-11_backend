e7b5ab94fb86a7659474efb9315cc968622f2208
Vedprakash Sinha <vedprakash4gs@gmail.com>
2023-05-06 15:22:20 +0530

    added

--- middleware/UsertoBookRelater.js
+++ middleware/UsertoBookRelater.js
@@ -2,11 +2,11 @@ const jwt = require("jsonwebtoken")
 const Relater=(req,res,next)=>{
     const token = req.headers.authorization
     console.log(token)
-    if(token){
+     if(token){
         const decoded = jwt.verify(token,"ved")
         req.body.userid=decoded.userid
         next()
-    }
+     }
 }
 
 module.exports={
